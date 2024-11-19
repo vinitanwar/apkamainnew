86bb127e1f24c9704b73921a5f39c52a9be57e2d
Vinod Tanwar <125426162+vinitanwar@users.noreply.github.com>
2024-11-19 16:23:12 +0530

    Update index.js

--- apkatrip/app/Component/common/index.js
+++ apkatrip/app/Component/common/index.js
@@ -1,5 +1,5 @@
 export const apilink = 'https://admin.gursimran.online/api/v1'
 //https://admin.gursimran.online
 export const imgurl = 'https://admin.gursimran.online/'
-export const development= "production";
+export const development= "";
 //http://localhost:8000
